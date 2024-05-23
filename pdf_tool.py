0b2b9579b1a4b81f6f9b884a64ae4f89b6cdfed1
guerrad49 <63264459+guerrad49@users.noreply.github.com>
2024-05-23 17:37:42 +0000

    renamed variables in cammelcase

--- pdf_tool.py
+++ pdf_tool.py
@@ -24,17 +24,17 @@ def parse_args():
     return args
 
 
-def split_pdf(base, parent_file):
-    reader = PdfReader(parent_file)
+def split_pdf(base, parentFile):
+    reader = PdfReader(parentFile)
     if reader.is_encrypted:
-        sys.exit('ERROR - File {} is encrypted.\n'.format(parent_file))
+        sys.exit('ERROR - File {} is encrypted.\n'.format(parentFile))
 
     for i,page in enumerate(reader.pages):
         writer = PdfWriter()
         writer.add_page(page)
-        out_name = '{}_page{:02d}.pdf'.format(parent_file[:-4], i+1)
-        out_name = os.path.join(base, out_name)
-        with open(out_name, 'wb') as f:
+        outName = '{}_page{:02d}.pdf'.format(parentFile[:-4], i+1)
+        outName = os.path.join(base, outName)
+        with open(outName, 'wb') as f:
             writer.write(f)
 
 
@@ -47,53 +47,53 @@ def merge_pdfs(base, in_list):
             sys.exit('ERROR - File {} is encrypted.\n'.format(file))
         merger.append(reader)
 
-    out_name = os.path.join(base, 'merged_file.pdf')
-    with open(out_name, 'wb') as f:
+    outName = os.path.join(base, 'merged_file.pdf')
+    with open(outName, 'wb') as f:
         merger.write(f)
 
 
-def reorder_pages(base, parent_file, in_order):
-    reader = PdfReader(parent_file)
+def reorder_pages(base, parentFile, inOrder):
+    reader = PdfReader(parentFile)
     if reader.is_encrypted:
-        sys.exit('ERROR - File {} is encrypted.\n'.format(parent_file))
+        sys.exit('ERROR - File {} is encrypted.\n'.format(parentFile))
 
-    in_order = [int(i)-1 for i in in_order.split(',')]
+    inOrder = [int(i)-1 for i in inOrder.split(',')]
     
-    if len(in_order) == 2:
+    if len(inOrder) == 2:
         order = list(range(len(reader.pages)))
-        order[in_order[0]],order[in_order[1]]=order[in_order[1]],order[in_order[0]]
-    elif len(in_order) > 2 and len(in_order) < len(reader.pages):
-        order = list(range(min(in_order)))
-        order += in_order
-        order += list(range(max(in_order)+1,len(reader.pages)))
-    elif len(in_order) == len(reader.pages):
-        order = in_order
+        order[inOrder[0]],order[inOrder[1]]=order[inOrder[1]],order[inOrder[0]]
+    elif len(inOrder) > 2 and len(inOrder) < len(reader.pages):
+        order = list(range(min(inOrder)))
+        order += inOrder
+        order += list(range(max(inOrder)+1,len(reader.pages)))
+    elif len(inOrder) == len(reader.pages):
+        order = inOrder
 
     writer = PdfWriter()
     for i in order:
         writer.add_page(reader.pages[i])
 
-    out_name = '{}_reordered.pdf'.format(parent_file[:-4])
-    out_name = os.path.join(base, out_name)
-    with open(out_name, 'wb') as f:
+    outName = '{}_reordered.pdf'.format(parentFile[:-4])
+    outName = os.path.join(base, outName)
+    with open(outName, 'wb') as f:
         writer.write(f)
 
 
-def remove_pages(base, parent_file, in_pages):
-    reader = PdfReader(parent_file)
+def remove_pages(base, parentFile, inPages):
+    reader = PdfReader(parentFile)
     if reader.is_encrypted:
-        sys.exit('ERROR - File {} is encrypted.\n'.format(parent_file))
+        sys.exit('ERROR - File {} is encrypted.\n'.format(parentFile))
 
-    pgs_to_rm = [int(i)-1 for i in in_pages.split(',')]
+    pgs_to_rm = [int(i)-1 for i in inPages.split(',')]
 
     writer = PdfWriter()
     for i in range(len(reader.pages)):
         if i not in pgs_to_rm:
             writer.add_page(reader.pages[i])
 
-    out_name = '{}_fixed.pdf'.format(parent_file[:-4])
-    out_name = os.path.join(base, out_name)
-    with open(out_name, 'wb') as f:
+    outName = '{}_fixed.pdf'.format(parentFile[:-4])
+    outName = os.path.join(base, outName)
+    with open(outName, 'wb') as f:
         writer.write(f)
 
     
@@ -107,8 +107,8 @@ if __name__ == '__main__':
     if args.split:
         split_pdf(basename, args.file)
     elif args.merge:
-        merge_list = [args.file] + args.merge
-        merge_pdfs(basename, merge_list)
+        mergeList = [args.file] + args.merge
+        merge_pdfs(basename, mergeList)
     elif args.reorder:
         reorder_pages(basename, args.file, args.reorder)
     elif args.remove:
