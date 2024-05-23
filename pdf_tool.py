7ec3e0ae84b852e7c683eac3a05e068bde3f3578
guerrad49 <63264459+guerrad49@users.noreply.github.com>
2024-05-23 23:20:30 +0000

    extracted repetative code to check_encryption and docstrings

--- pdf_tool.py
+++ pdf_tool.py
@@ -24,93 +24,146 @@ def parse_args():
     return args
 
 
-def split_pdf(base, parentFile):
-    reader = PdfReader(parentFile)
-    if reader.is_encrypted:
-        sys.exit('ERROR - File {} is encrypted.\n'.format(parentFile))
+def split(dirPath, fileName):
+    """
+    Split and save all PDF pages into separate PDF.
+    
+    Parameters
+    ----------
+    dirPath: str
+        The full directory path of the PDF file
+    fileName: str
+        The name of the PDF file
+    """
 
-    for i,page in enumerate(reader.pages):
+    reader = PdfReader(fileName)
+
+    for i, page in enumerate(reader.pages):
         writer = PdfWriter()
         writer.add_page(page)
-        outName = '{}_page{:02d}.pdf'.format(parentFile[:-4], i+1)
-        outName = os.path.join(base, outName)
-        with open(outName, 'wb') as f:
+        outName = '{}_page{:02d}.pdf'.format(fileName[:-4], i+1)
+        outPath = os.path.join(dirPath, outName)
+        with open(outPath, 'wb') as f:
             writer.write(f)
 
 
-def merge_pdfs(base, in_list):
-    merger = PdfMerger()
+def merge(dirPath, filesToMerge):
+    """
+    Merge a list of PDFs into one.
 
-    for file in in_list:
+    Parameters
+    ----------
+    dirPath: str
+        The full directory path of the PDF file
+    filesToMerge: list
+        The list of PDF names to merge
+    """
+    
+    merger = PdfMerger()
+    for file in filesToMerge:
         reader = PdfReader(file)
-        if reader.is_encrypted:
-            sys.exit('ERROR - File {} is encrypted.\n'.format(file))
         merger.append(reader)
 
-    outName = os.path.join(base, 'merged_file.pdf')
+    outName = os.path.join(dirPath, 'merged_file.pdf')
     with open(outName, 'wb') as f:
         merger.write(f)
 
 
-def reorder_pages(base, parentFile, inOrder):
-    reader = PdfReader(parentFile)
-    if reader.is_encrypted:
-        sys.exit('ERROR - File {} is encrypted.\n'.format(parentFile))
+def reorder(dirPath, fileName, order):
+    """
+    Merge a list of PDFs into one.
 
-    inOrder = [int(i)-1 for i in inOrder.split(',')]
+    Parameters
+    ----------
+    dirPath: str
+        The full directory path of the PDF file
+    fileName: str
+        The name of the PDF file
+    order: str
+        The comma-separated string with the new page order
+    """
+
+    reader = PdfReader(fileName)
+
+    order = [int(i)-1 for i in order.split(',')]
     
-    if len(inOrder) == 2:
+    if len(order) == 2:
         order = list(range(len(reader.pages)))
-        order[inOrder[0]],order[inOrder[1]]=order[inOrder[1]],order[inOrder[0]]
-    elif len(inOrder) > 2 and len(inOrder) < len(reader.pages):
-        order = list(range(min(inOrder)))
-        order += inOrder
-        order += list(range(max(inOrder)+1,len(reader.pages)))
-    elif len(inOrder) == len(reader.pages):
-        order = inOrder
+        order[order[0]] , order[order[1]] = \
+            order[order[1]] , order[order[0]]
+    elif (len(order) > 2) and (len(order) < len(reader.pages)):
+        order = list(range(min(order)))
+        order += order
+        order += list(range(max(order)+1,len(reader.pages)))
+    elif len(order) == len(reader.pages):
+        order = order
 
     writer = PdfWriter()
     for i in order:
         writer.add_page(reader.pages[i])
 
-    outName = '{}_reordered.pdf'.format(parentFile[:-4])
-    outName = os.path.join(base, outName)
-    with open(outName, 'wb') as f:
+    outName = '{}_reordered.pdf'.format(fileName[:-4])
+    outPath = os.path.join(dirPath, outName)
+    with open(outPath, 'wb') as f:
         writer.write(f)
 
 
-def remove_pages(base, parentFile, inPages):
-    reader = PdfReader(parentFile)
-    if reader.is_encrypted:
-        sys.exit('ERROR - File {} is encrypted.\n'.format(parentFile))
+def remove(dirPath, fileName, pageNums):
+    """
+    Remove given files from PDF.
 
-    pgs_to_rm = [int(i)-1 for i in inPages.split(',')]
+    Parameters
+    ----------
+    dirPath: str
+        The full directory path of the PDF file
+    fileName: str
+        The name of the PDF file
+    pageNums: str
+        The comma-separated string with page numbers to remove
+    """
+
+    reader = PdfReader(fileName)
+
+    pgsToRemove = [int(i)-1 for i in pageNums.split(',')]
 
     writer = PdfWriter()
-    for i in range(len(reader.pages)):
-        if i not in pgs_to_rm:
+    for i, _ in enumerate(reader.pages):
+        if i not in pgsToRemove:
             writer.add_page(reader.pages[i])
 
-    outName = '{}_fixed.pdf'.format(parentFile[:-4])
-    outName = os.path.join(base, outName)
-    with open(outName, 'wb') as f:
+    outName = '{}_fixed.pdf'.format(fileName[:-4])
+    outPath = os.path.join(dirPath, outName)
+    with open(outPath, 'wb') as f:
         writer.write(f)
+
+
+def check_encryption(fileList):
+    """Checks encryption and exits program if so."""
+    
+    for f in fileList:
+        reader = PdfReader(f)
+        if reader.is_encrypted:
+            msg = 'ERROR - File {} is encrypted.\n'.format(f)
+            sys.exit(msg)
 
     
 #=============================================================
 
 if __name__ == '__main__':
-    
     args = parse_args()
     basename = os.path.dirname(args.file)
 
     if args.split:
-        split_pdf(basename, args.file)
+        check_encryption([args.file])
+        split(basename, args.file)
     elif args.merge:
         mergeList = [args.file] + args.merge
-        merge_pdfs(basename, mergeList)
+        check_encryption(mergeList)
+        merge(basename, mergeList)
     elif args.reorder:
-        reorder_pages(basename, args.file, args.reorder)
+        check_encryption([args.file])
+        reorder(basename, args.file, args.reorder)
     elif args.remove:
-        remove_pages(basename, args.file, args.remove)
+        check_encryption([args.file])
+        remove(basename, args.file, args.remove)
  
